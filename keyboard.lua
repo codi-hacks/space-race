dcdcd7a7d2ea2e9d5c50f9bef37b0b76197c9c76
Trevor Corcoran <supershadowplay@gmail.com>
2021-11-19 08:50:59 -0500

    Keyboard.lua now reads scancodes

--- keyboard.lua
+++ keyboard.lua
@@ -20,10 +20,10 @@ function keyboard.move(time, obj)
     local ratioy = (topy - obj.body:getY()) * 0.04
 
     -- Fly spaceship forwards and backwards
-    if love.keyboard.isDown('w') then
+    if love.keyboard.isScancodeDown('w') then
         obj.body:applyForce(ratiox * movementForce, ratioy * movementForce)
     end
-    if love.keyboard.isDown('s') then
+    if love.keyboard.isScancodeDown('s') then
         obj.body:applyForce(ratiox * movementForce * -0.25, ratioy * movementForce * -0.25)
     end
 
@@ -31,10 +31,10 @@ function keyboard.move(time, obj)
     local angVel = player.body:getAngularVelocity()
     local maxAngVel = 2
     if angVel > -maxAngVel and angVel < maxAngVel then
-        if love.keyboard.isDown('a') then
+        if love.keyboard.isScancodeDown('a') then
             obj.body:applyTorque(-600)
         end
-        if love.keyboard.isDown('d') then
+        if love.keyboard.isScancodeDown('d') then
             obj.body:applyTorque(600)
         end
     end
@@ -42,37 +42,37 @@ function keyboard.move(time, obj)
     -- The rest of this function is completely optional
     -- (and was left in because it may be useful during development)
     -- Move object via arrow keys with directional force
-    if love.keyboard.isDown('up') then
+    if love.keyboard.isScancodeDown('up') then
         obj.body:applyForce(0, -movementForce)
     end
-    if love.keyboard.isDown('down') then
+    if love.keyboard.isScancodeDown('down') then
         obj.body:applyForce(0, movementForce)
     end
-    if love.keyboard.isDown('right') then
+    if love.keyboard.isScancodeDown('right') then
         obj.body:applyForce(movementForce, 0)
     end
-    if love.keyboard.isDown('left') then
+    if love.keyboard.isScancodeDown('left') then
         obj.body:applyForce(-movementForce, 0)
     end
 
     -- Move using arrow keys (while holding shift) with "teleportation".
     -- A.K.A. adjusting the x/y of an object.
-    if love.keyboard.isDown('lshift') then
+    if love.keyboard.isScancodeDown('lshift') then
         obj.body:setLinearVelocity(0, 0)
         obj.body:setAngularVelocity(0)
         local movespeed = 200
         local xpos = obj.body:getX()
         local ypos = obj.body:getY()
-        if love.keyboard.isDown('up') then
+        if love.keyboard.isScancodeDown('up') then
             ypos = ypos - time * movespeed
         end
-        if love.keyboard.isDown('down') then
+        if love.keyboard.isScancodeDown('down') then
             ypos = ypos + time * movespeed
         end
-        if love.keyboard.isDown('left') then
+        if love.keyboard.isScancodeDown('left') then
             xpos = xpos - time * movespeed
         end
-        if love.keyboard.isDown('right') then
+        if love.keyboard.isScancodeDown('right') then
             xpos = xpos + time * movespeed
         end
 
@@ -81,7 +81,7 @@ function keyboard.move(time, obj)
     end
 
     -- Induce crazy spin
-    if love.keyboard.isDown('p') then
+    if love.keyboard.isScancodeDown('p') then
         obj.body:setAngularVelocity(20)
     end
 end
