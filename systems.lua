fb7a1e6a3565c41eb935622342362681ccbbb952
Jay Thomas <jay@gfax.ch>
2021-12-02 22:22:48 -0500

    Remove math.floor causing jittering

--- systems.lua
+++ systems.lua
@@ -79,40 +79,37 @@ systems.ControlPlayer = System(
 )
 
 systems.UpdateCamera = System(
-    {'-isControlled', 'body', 'size'},
-    function(body, size)
+    {'-isControlled', 'body'},
+    function(body)
         local player_pos_x, player_pos_y = body:getPosition()
+
+        local camera_pos_x = player_pos_x - 400
+        local camera_pos_y = player_pos_y - 300
+
+        --[[
+        local camera_pos_x, camera_pos_y = Camera.get_position()
         local player_height = size
         local player_width = size
-        local camera_pos_x, camera_pos_y = Camera.get_position()
 
         local boundary_bottom = Camera.get_boundary_bottom()
         local boundary_top = Camera.get_boundary_top()
-        local boundary_left = Camera.get_boundary_left()
-        local boundary_right = Camera.get_boundary_right()
 
-        camera_pos_y = player_pos_y - 300
-        camera_pos_x = player_pos_x - 400
-        --[[ Keep these in for when a map boundary is put in place!
         if player_pos_y < boundary_top then
-            camera_pos_y = camera_pos_y - (boundary_top - player_pos_y) 
+            camera_pos_y = camera_pos_y - (boundary_top - player_pos_y)
         elseif player_pos_y + player_height > boundary_bottom then
             camera_pos_y = camera_pos_y - (boundary_bottom - (player_pos_y + player_height))
         end
+
+        local boundary_left = Camera.get_boundary_left()
+        local boundary_right = Camera.get_boundary_right()
 
         if player_pos_x < boundary_left then
             camera_pos_x = camera_pos_x - (boundary_left - player_pos_x)
         elseif player_pos_x + player_width > boundary_right then
             camera_pos_x = camera_pos_x - (boundary_right - (player_pos_x + player_width))
-        end
-        ]]--
+        end]]--
 
-        print(camera_pos_x, camera_pos_y)
-        -- Floor the values to avoid screen-tearing floats
-        Camera.set_position(
-            math.floor(camera_pos_x),
-            math.floor(camera_pos_y)
-        )
+        Camera.set_position(camera_pos_x, camera_pos_y)
     end
 )
 
