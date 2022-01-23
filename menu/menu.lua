395cab2a4b78d5e6fb323ef3f15ec0aae29a48cb
Trevor Corcoran <supershadowplay@gmail.com>
2022-01-23 14:31:27 -0500

    Fancy libre font

--- menu/menu.lua
+++ menu/menu.lua
@@ -85,10 +85,10 @@ menu.draw = function()
 
     -- Draw text
     if blink then
-        love.graphics.print(mapList[mapSelect].displayName, corner[1] + 55, corner[2] + 420, 0, 4, 4)
+        love.graphics.print(mapList[mapSelect].displayName, corner[1] + 55, corner[2] + 420, 0, 2, 2)
     end
-    love.graphics.print('Current Map:\n' .. mapList[state.activeMap].displayName, corner[1] + 460, corner[2] + 450, 0, 4, 4)
-    love.graphics.print('SPACE RACE', corner[1] + 25, corner[2] + 100, 0, 4, 4)
+    love.graphics.print('Current Map:\n' .. mapList[state.activeMap].displayName, corner[1] + 400, corner[2] + 450, 0, 2, 2)
+    love.graphics.print('SPACE RACE', corner[1] + 25, corner[2] + 100, 0, 2, 2)
 
     --[[if blink == true then
         love.graphics.setColor({1, 0, 0, 1})
