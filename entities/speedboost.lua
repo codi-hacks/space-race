1cbe98c0c1ad9a3f7214168a02cb96cbf1e0f417
Wyatt J Herkamp <wherkamp@kingtux.me>
2022-01-26 10:37:58 -0500

    Alright added a speed boost

---
+++ entities/speedboost.lua
@@ -0,0 +1,17 @@
+return function()
+    return {
+        body = {
+            mass = 0
+        },
+        speed_boost = 1,
+        shape = {
+            type = 'rectangle',
+            width =64,
+            height=64
+        },
+        spritesheet = {
+            image = 'speedboost',
+
+        }
+    }
+end
--- services/textures.lua
+++ services/textures.lua
@@ -4,6 +4,7 @@ textures.load = function()
     textures.spaceship = love.graphics.newImage("/assets/sprites/ship.png")
     textures.star = love.graphics.newImage("/assets/sprites/star.png")
     textures.planet = love.graphics.newImage('assets/sprites/planet.png')
+    textures.speedboost = love.graphics.newImage('assets/sprites/speedboost.png')
 end
 
 return textures
