98b8ab739a27e4644a9552424c538fed67108b84
Trevor Corcoran <supershadowplay@gmail.com>
2022-01-31 22:46:59 -0500

    Added Jon's textures to texture service.

--- services/textures.lua
+++ services/textures.lua
@@ -4,6 +4,12 @@ textures.load = function()
     textures.spaceship = love.graphics.newImage("/assets/sprites/ship.png")
     textures.star = love.graphics.newImage("/assets/sprites/star.png")
     textures.planet = love.graphics.newImage('assets/sprites/planet.png')
+
+    -- Assorted spaceships by Jon
+    textures.spaceship2 = love.graphics.newImage('/assets/sprites/ship2.png')
+    textures.shuttle = love.graphics.newImage('/assets/sprites/shuttle.png')
+    textures.shuttle_no_boosters = love.graphics.newImage('/assets/sprites/shuttle_no_boosters.png')
+    textures.ufo = love.graphics.newImage('/assets/sprites/shuttle_no_boosters.png')
 end
 
 return textures
