dc98c565f7e8399e04a5d032e32388d802444643
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2022-02-01 22:32:07 -0500

    Merge (#30)
    * Lua Check Cleanup  (#29)
    
    * Clean Up
    
    * Clean Up
    
    * Repaired Comment
    
    * If I am being honest. I really don't know what I am doing
    
    * To Be honest. Not sure why we were looping in the first place
    
    * Decided to go ahead and cleanup the debug
    
    * Lua Check

--- services/textures.lua
+++ services/textures.lua
@@ -4,16 +4,19 @@ textures.load = function()
     textures.spaceship = love.graphics.newImage("/assets/sprites/ship.png") -- Ship Index 1 J.R.C 2/1/22
     textures.star = love.graphics.newImage("/assets/sprites/star.png")
     textures.planet = love.graphics.newImage('assets/sprites/planet.png')
-
     -- Wyatt's Beautiful spedboost
     textures.speedboost = love.graphics.newImage('assets/sprites/speedboost.png')
-
      -- Assorted spaceships by Jon
-     textures.ship_spaceship_2 = love.graphics.newImage('/assets/sprites/ship_ship_2.png')-- Ship Index 2 J.R.C 2/1/22
-     textures.ship_ufo = love.graphics.newImage('/assets/sprites/ship_ufo.png') -- Ship Index 3 J.R.C 2/1/22
-     textures.ship_shuttle = love.graphics.newImage('/assets/sprites/ship_shuttle.png') -- Ship Index 4 J.R.C 2/1/22
-     textures.ship_shuttle_no_boosters = love.graphics.newImage('/assets/sprites/ship_shuttle_no_boosters.png') -- Ship Index 5 J.R.C 2/1/22
-     textures.ship_purple = love.graphics.newImage('/assets/sprites/ship_purple.png') -- Ship Index 6 J.R.C 2/1/22
+    -- Ship Index 2 J.R.C 2/1/22
+     textures.ship_spaceship_2 = love.graphics.newImage('/assets/sprites/ship_ship_2.png')
+    -- Ship Index 3 J.R.C 2/1/22
+     textures.ship_ufo = love.graphics.newImage('/assets/sprites/ship_ufo.png')
+    -- Ship Index 4 J.R.C 2/1/22
+     textures.ship_shuttle = love.graphics.newImage('/assets/sprites/ship_shuttle.png')
+    -- Ship Index 5 J.R.C 2/1/22
+     textures.ship_shuttle_no_boosters = love.graphics.newImage('/assets/sprites/ship_shuttle_no_boosters.png')
+    -- Ship Index 6 J.R.C 2/1/22
+     textures.ship_purple = love.graphics.newImage('/assets/sprites/ship_purple.png')
 
 end
 
