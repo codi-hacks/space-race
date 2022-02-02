95cf6e9ac6a7a2d05bb4e45a0684947df63a5232
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2022-02-01 21:16:33 -0500

    Decided to go ahead and cleanup the debug

--- systems/DebugPlayer.lua
+++ systems/DebugPlayer.lua
@@ -24,32 +24,29 @@ local function debug(entity)
         local pos_x = state.camera.pos_x
         local pos_y = state.camera.pos_y
         love.graphics.setColor({ 1, 1, 1, 1 })
-
+        -- Time
         local clock_display = 'Time: ' .. roundOff(love.seconds)
         love.graphics.print(clock_display, pos_x, pos_y, 0, 0.9, 0.9)
-
+        -- The Position
         local playerPosition = 'X/Y Pos: ' .. roundOff(entity.body:getX()) .. '/' .. roundOff(entity.body:getY())
         love.graphics.print(playerPosition, pos_x, pos_y + 25, 0, 0.9, 0.9)
+        -- Velocity of the Player
         local currentVelocityX, currentVelocityY = entity.body:getLinearVelocity()
         local playerVelocity = 'X/Y Vel: ' .. roundOff(currentVelocityX) .. '/' .. roundOff(currentVelocityY)
         love.graphics.print(playerVelocity, pos_x, pos_y + 50, 0, 0.9, 0.9)
-
+        -- Camera Pos
         local campos = 'Camera: ' .. roundOff(pos_x) .. ':' .. roundOff(pos_y)
         love.graphics.print(campos, pos_x, pos_y + 100, 0, 0.9, 0.9)
-
-        -- Direction line and hitbox
-        local lastColor = { love.graphics.getColor() }
-        love.graphics.setColor({ 1, 0, 0, 1 })
         -- VelocityArrow Line For the Player
-        local velocityArrow = { entity.body:getX() + currentVelocityX, entity.body:getY() + currentVelocityY }
-        love.graphics.line(entity.body:getX(), entity.body:getY(), velocityArrow[1], velocityArrow[2])
-
         local velocityArrowX = entity.body:getX() + currentVelocityX
         local velocityArrowY = entity.body:getY() + currentVelocityY
-
-        love.graphics.setColor(lastColor)
+        -- Location of arrow
         love.graphics.print('VelocityArrow: ' .. roundOff(velocityArrowX) .. '/'
             .. roundOff(velocityArrowY), pos_x, pos_y + 125, 0, 0.9, 0.9)
+        -- Actually Draw the Line
+        love.graphics.setColor({ 1, 0, 0, 1 })
+        love.graphics.line(entity.body:getX(), entity.body:getY(), velocityArrowX,velocityArrowY)
+
     end
 end
 
