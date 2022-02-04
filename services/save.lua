19dbb163ba8941910ebeeab16dbc539161b712f0
Trevor Corcoran <supershadowplay@gmail.com>
2022-02-04 10:10:15 -0500

    Use love2D filesystem methods instead

--- services/save.lua
+++ services/save.lua
@@ -6,7 +6,7 @@ save.read = function()
     -- Load data from a file. It's just a lua table that gets loaded into memory with:
     -- local data = {credits = <number>, time = <string>, lastMap = <number>}
 
-    local f = assert(loadfile('maps/saveData.lua')); f()
+    local f = love.filesystem.load('maps/saveData.lua'); f()
     state.credits = data.credits
     state.activeMap = data.lastMap
     local time = data.time -- Placeholder
@@ -21,12 +21,11 @@ save.write = function()
     data.credits = state.credits
     data.time = '2:33:44'
     data.mapID = state.activeMap
-    local newdata = 'data = {credits = ' .. data.credits .. ', time = \'' .. data.time .. '\', lastMap = ' .. data.mapID .. '}'
-
-    -- Write the data to the file.
-    fopen = io.open('maps/saveData.lua', 'w')
-    fopen:write(newdata)
-    fopen:close()
+    local newdata = 'data = {credits = ' .. data.credits
+    .. ', time = \'' .. data.time
+    .. '\', lastMap = ' .. data.mapID
+    .. '}'
+    love.filesystem.write('maps/saveData.lua', newdata)
 end
 
 return save
