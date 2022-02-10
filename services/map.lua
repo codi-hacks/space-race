10b1008e812f02b8064e5d01ccff58ddc0818542
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2022-02-10 17:20:28 -0500

    Undo 'Jay only wants one exposed load function'

--- services/map.lua
+++ services/map.lua
@@ -119,10 +119,17 @@ return {
     -- @param {string} map_name - name of map to draw
     -- @return {nil}
     draw = draw,
+
     -- Return active map's pixel dimensions
     -- @param {number} width
     -- @param {number} height
     get_dimensions = get_dimensions,
+    load = load,
     loadMap = loadMap,
-
+    --- Set loaded images and quads for a given map to nil.
+    -- would have a map name of "general".
+    -- @param {string} map_name - name of the tmx file to load
+    -- @param {table} ship_index - index of ship to spawn
+    -- @return {table} - parsed Tiled map data
+    unload = unload
 }
