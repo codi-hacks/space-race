f5695ccad3073f8c7e2f36fcc5fd33c10140381e
Jonathan Cavaliere <Cavalierejon@gmail.com>
2022-03-08 15:40:52 -0500

    Merge resolution preparation

--- services/sounds.lua
+++ services/sounds.lua
@@ -5,7 +5,6 @@ sounds.engine = love.audio.newSource('/assets/sounds/engine_normal.mp3', 'stream
 sounds.braking = love.audio.newSource('/assets/sounds/engine_braking.mp3', 'stream')
 sounds.chirp_up = love.audio.newSource('/assets/sounds/chirp_up.mp3', 'stream')
 sounds.chirp_down = love.audio.newSource('/assets/sounds/chirp_down.mp3', 'stream')
-sounds.chirp_deny = love.audio.newSource('/assets/sounds/chirp_deny.mp3', 'stream')
 
 sounds.menu_click = love.audio.newSource('/assets/sounds/Flashpoint001d.flac', 'stream')
 
